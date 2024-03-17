5f4f0029f59736d918ade436f13912a78b2f57ed
Hellmakima <154533744+Hellmakima@users.noreply.github.com>
2024-03-17 21:43:18 +0530

    Update ship server.py
    massively improved the broadcast system. maybe ill make it a dictionary next

--- ship server.py
+++ ship server.py
@@ -1,51 +1,58 @@
-# Server (Broadcasting)
+# Server
 import socket
 import threading
 import time
 import json
 
-count = 0
-player = []
-client_list = []
-client_broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+player_data = []
+player_b_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+player_b_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-server_address = ('<your_server_ip>', 12345)
+fps = 0.017  # 1/fps
+
 
 def listen_to_client():
-    global count
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.bind(('0.0.0.0', 12345))
+    player_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    player_socket.bind(('0.0.0.0', 12345))
 
     while True:
-        data, client_address = client_socket.recvfrom(1024)
+        data, player_address = player_socket.recvfrom(124)
         data = data.decode()
-        print(f"Received message from client at {client_address}: '{data}'")
+        print(f"Received message from client at {player_address}: '{data}'")
         if data == 'new':
-            client_list.append(client_address)
-            player.append((count, 0, 0))
-            count += 1
+            player_data.append((len(player_data), 0, 0))
+        elif data.split(' ')[0] == 'bye':
+            print('bye client')
+            uid = data.split()[1]
+            for player in player_data:
+                if player[0] == uid:
+                    player_data.remove(player)
+                    break
         else:
             msg = eval(data)
-            player[msg[0]] = (msg[1], msg[2])
+            for i, player in enumerate(player_data):
+                if player[0] == msg[0]:
+                    player_data[i] = tuple(msg)
+                    break
+
+
+threading.Thread(target=listen_to_client, daemon=True).start()
+
+
 def send_data():
-    global player, client_list
-    json_to_send = json.dumps(player)
-    for client in client_list:
-        client_broadcast_socket.sendto(json_to_send.encode(), (client[0],12344))
+    global player_data
+    json_to_send = json.dumps(player_data)
+    # print(f'sent{player_data}')
+    player_b_socket.sendto(
+        json_to_send.encode(), ('<broadcast>', 12344))
 
-def broadcast():
-    global count
-    while True:
-        time.sleep(1)
-        client_broadcast_socket.sendto(str(count).encode(), ('<broadcast>', 12333))
 
-client_thread = threading.Thread(target = listen_to_client, daemon = True)
-client_thread.start()
-broadcast_thread = threading.Thread(target = broadcast, daemon = True)
-broadcast_thread.start()
+def run_player():
+    import ship_player
 
+
+# threading.Thread(None, target=run_player, daemon=True).start()
 while True:
-    # time.sleep(.01)
-    print(player)
+    time.sleep(fps)
+    # print(player_data)
     send_data()
