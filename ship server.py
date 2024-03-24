d8c662639b96e3b27db943a7a0c0518d855d3db5
Hellmakima <154533744+Hellmakima@users.noreply.github.com>
2024-03-24 11:16:43 +0530

    Update ship server.py
    added bullets

--- ship server.py
+++ ship server.py
@@ -3,8 +3,11 @@ import socket
 import threading
 import time
 import json
+import pygame as p
 
+width2, height2 = 500, 500
 player_data = []
+bullets = []
 player_b_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 player_b_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
@@ -20,16 +23,20 @@ def listen_to_client():
         data = data.decode()
         print(f"Received message from client at {player_address}: '{data}'")
         if data == 'new':
-            player_data.append((len(player_data), 0, 0))
+            player_data.insert(len(player_data), (len(player_data), 0, 0))
         elif data.split(' ')[0] == 'bye':
-            print('bye client')
             uid = data.split()[1]
+            print(f'uid {uid} left')
             for player in player_data:
-                if player[0] == uid:
+                if player[0] == int(uid):
                     player_data.remove(player)
                     break
         else:
             msg = eval(data)
+            if msg[0] == 'bullet':
+                bullets.append(msg[1:])
+                # print(msg)
+                continue
             for i, player in enumerate(player_data):
                 if player[0] == msg[0]:
                     player_data[i] = tuple(msg)
@@ -41,18 +48,37 @@ threading.Thread(target=listen_to_client, daemon=True).start()
 
 def send_data():
     global player_data
+    player_data.append(bullets)
     json_to_send = json.dumps(player_data)
-    # print(f'sent{player_data}')
+    print(f'sent{player_data}')
     player_b_socket.sendto(
         json_to_send.encode(), ('<broadcast>', 12344))
+    player_data.pop()
 
 
 def run_player():
     import ship_player
 
 
+def update_bullets():
+    for bullet in bullets:
+        dir = p.Vector2(bullet[3], bullet[4])
+        try:
+            dir.normalize_ip()
+        except:
+            bullets.remove(bullet)
+            continue
+        # end = click + dir * 40
+        bullet[1] += 4 * dir.x
+        bullet[2] += 4 * dir.y
+        if bullet[1] > width2 or bullet[1] < -width2 or bullet[2] > height2 or bullet[2] < -height2:
+            bullets.remove(bullet)
+            continue
+
+
 # threading.Thread(None, target=run_player, daemon=True).start()
 while True:
     time.sleep(fps)
+    update_bullets()
     # print(player_data)
     send_data()
